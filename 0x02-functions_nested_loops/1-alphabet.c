2005a85b31ca0262256ba4d6dde49b9183e49cac
Nkoba-Sage <matovunkobaismael10@gmail.com>
2022-09-14 11:42:19 -0700

    huw

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,11 @@
 
 /**
  * print_alphabet - print the alphabet in lowercase
- * 
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int n;
-	
 	for (n = 'a'; n <= 'z'; n++)
 	{
 		_putchar(n);
