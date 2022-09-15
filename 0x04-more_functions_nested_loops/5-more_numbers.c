838e30707ad4ddbbb7e9a37b53df0a5643d55c86
Nkoba-Sage <111007208+Nkoba-Sage@users.noreply.github.com>
2022-09-15 23:16:27 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,19 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-* more_numbers - prints 10 lines of 1-14
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: void. no return.
+* more_numbers - prints 10 times the numbers, from 0 to 14
+* followed by a new line
 */
-
 void more_numbers(void)
 {
-	int i = 0, j = 0;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar((j % 10) + '0');
-		}
-		_putchar('\n');
-	}
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+if (j >= 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
 }
