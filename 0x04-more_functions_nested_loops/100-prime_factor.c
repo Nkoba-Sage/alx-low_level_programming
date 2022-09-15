68f48b761d287350e3db6c5e0f7f0f65a3b1e6b9
Nkoba-Sage <111007208+Nkoba-Sage@users.noreply.github.com>
2022-09-15 23:24:17 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,48 +1,32 @@
 #include <stdio.h>
 #include <math.h>
-
 /**
-* main - finds the largest prime
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: 0 if no errors
-* A: Sqrt is a double by default so I did this to get rid of decimals.
-* B: While the number is bigger than the sqrt of the original number.
-* Why? Because if the number is prime my loop will go until the number.
-* If it has a factor then we know the largest possible prime factor
-* it can have, is less than the sqrt of the original number.
-* Math concept here, don’t need to check full length
-* saves lots of time to log n I think. This is a hard concept
-* UPDATE: After test cases, i changed it from number > sqrt to
-* number > i because some small test cases did not pass 
-* But with the change it should work
-* C: If the current number can be modulo’d by i, check if i is bigger
-* than biggest. We then chunk the number by the i, reducing it and
-* running the loop with the same i to further chuck it down, if possible.
-* This makes number smaller and smaller which will be checked
-* in the outer while loop. This helps check for prime and factors.
-* D: Increase i because number was no longer modulo’d by i.
-* I could have put i+=2 to half the speed of the program since we do
-* not need to check for even modulus since 2 handles that, but I would
-* have to refactor my code, starting with a i = 2 loop then start
-* my code with i=3. In this current code of mine, I have i = 2.
+* main - finds and prints the largest prime factor of the number 612852475143
+* followed by a new line
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	unsigned long i = 2;
-	unsigned long biggest = 0;
-	unsigned long number = 612852475143;
-
-	while (number > i)/* B */
-	{
-		while (number % i == 0)/* C */
-		{
-			if (i > biggest)
-				biggest = i;
-			number = number / i;
-		}
-		i++;/* D */
-	}
-	printf("%lu\n", biggest);
-	return (0);
+long int n;
+long int max;
+long int i;
+n = 612852475143;
+max = -1;
+while (n % 2 == 0)
+{
+max = 2;
+n /= 2;
+}
+for (i = 3; i <= sqrt(n); i = i + 2)
+{
+while (n % i == 0)
+{
+max = i;
+n = n / i;
+}
+}
+if (n > 2)
+max = n;
+printf("%ld\n", max);
+return (0);
 }
