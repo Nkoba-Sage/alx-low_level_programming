a7b11699cfaf0c02edb0b82a8d702d40671bb016
Nkoba-Sage <111007208+Nkoba-Sage@users.noreply.github.com>
2022-09-15 23:20:33 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,33 +1,26 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-* print_diagonal - prints a diagonal line with new line
-* @n: an int given by main
-*
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: void. no return.
+* print_diagonal - draws a diagonal line on the terminal
+* @n: number of times the character \ should be printed
 */
-
 void print_diagonal(int n)
 {
-	int i, j;
-
-	if (n <= 0)/* if 0 or negative just newline */
-		_putchar('\n');
-	for (i = 0; i < n; i++)/* we want the amount of rows equal to n */
-	{
-	/*for each column we start at the origin 0,0*/
-	/* when i did j = +i i get a different pattern, the up right pattern*/
-	/* i is the inital spacing needed eg. for 10*/
-	/* j is the counter. j = 10 - 0 , 10 is not less than 10, so skip*/
-	/* now j = 9, 9 < 10 true, do once */
-	/* omg i am a genius UPDATENVM. im an idiot. i could have  */
-	/* just done a i = j check and insert \ if true and space if not*/
-		for (j = n - i; j < n; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
-		_putchar('\n');
-	}
+if (n <= 0)
+{
+_putchar('\n');
+} else
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (j == i)
+_putchar('\\');
+else if (j < i)
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
 }
