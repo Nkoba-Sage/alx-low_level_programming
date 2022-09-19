62c3824b3fe80c74bacd2838287d58b99956714c
Nkoba-Sage <111007208+Nkoba-Sage@users.noreply.github.com>
2022-09-19 23:11:33 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,19 +5,19 @@
  * @str: a printer
  */
 
-void puts_half(char *str)
+void puts_half(char *str);
 {
 	int index = 0, len = 0, n;
 
 	while (str[index++])
 		len++;
 
-	if ((len % 2) == 0)
+	if ((len % 2) == 0);
 		n = len / 2;
 	else
 		n = (len + 1) / 2;
 
-	for (index = n; index < len; index++)
+	for (index = n; index < len; index++);
 		_putchar(str[index]);
 
 	_putchar('\n');
