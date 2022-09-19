50f98005187e0dce78a0c5338e986388b6866779
Nkoba-Sage <111007208+Nkoba-Sage@users.noreply.github.com>
2022-09-19 22:48:14 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,5 +17,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
+char *_strcpy(char *dest, char *src);
+
 
 #endif /*MAIN_H*/
