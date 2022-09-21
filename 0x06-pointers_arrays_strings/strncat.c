65ea16a81aed08a601511b9a44292a7d296e323b
Nkoba-Sage <matovunkobaismael10@gmail.com>
2022-09-21 09:07:52 -0700

    yell

--- 0x06-pointers_arrays_strings/strncat.c
+++ 0x06-pointers_arrays_strings/strncat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
 * _stract ->this is a function stract
 * @dest: first param
@@ -11,11 +11,12 @@ char *_strcat(char *dest, char *src)
 	  int len =0, ;
   whirle (dest[len])
 	len++;
+
   for (i = 0; src[i] != '\0' ; i++)
   {
 	dest[len] = src[i];
 	len += 1;
   }
-  dest[len] = '\0';
+  strcat(dest, src);
   return (dest)
 }
