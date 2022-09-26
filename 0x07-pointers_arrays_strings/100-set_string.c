2843a934221c9a494edbea359890b45e20b21d3e
Nkoba-Sage <111007208+Nkoba-Sage@users.noreply.github.com>
2022-09-26 23:25:19 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,4 @@
-#include "main.h"
-
-/**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: pointer char
- */
-
-void set_string(char **s, char *to)
-{
-	*s = to;
-}
+/*
+*File: 100-set_string.c
+*Auth: Nkoba
+*/
